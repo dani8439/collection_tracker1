a4926aa85c0d6a85b877574526eca8b2abc76ee8
Dani Schuhman <dani8439@aol.com>
2018-10-16 14:43:41 +0000

    building out pieces controller & views

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,2 +1,35 @@
 class PiecesController < ApplicationController
+
+  get '/pieces' do
+    @piece = Piece.all
+    @user = User.find_by(params[:id])
+    if logged_in?
+      erb :'/pieces/index'
+    else
+      redirect :'/'
+    end
+  end
+
+  get '/pieces/new' do
+    if logged_in?
+      erb :'pieces/new'
+    else
+      redirect :'/login'
+    end
+  end
+
+  get '/pieces/:id' do
+  end
+
+  get '/pieces/:id/edit' do
+  end
+
+  post '/pieces' do
+  end
+
+  patch '/pieces/:id' do
+  end
+
+  delete '/pieces/:id/delete' do
+  end
 end
