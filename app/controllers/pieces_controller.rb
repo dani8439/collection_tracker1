c216e956140507ff1cb5ac90e17e201169d753d2
Dani Schuhman <dani8439@aol.com>
2018-10-16 16:17:53 +0000

    pieces controller routes

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -19,17 +19,44 @@ class PiecesController < ApplicationController
   end
 
   get '/pieces/:id' do
+    if logged_in?
+      @piece = Piece.find_by_id(params[:id])
+      erb :'pieces/show'
+    else
+      redirect :'/login'
+    end
   end
 
   get '/pieces/:id/edit' do
+    if logged_in?
+      @piece = Piece.find_by_id(params[:id])
+      erb :'/pieces/edit'
+    else
+      redirect :'/login'
+    end
   end
 
   post '/pieces' do
+    if params[:name] == "" || params[:size] == "" || params[:quantity] == "" || params[:pattern] == ""
+      redirect :'/pieces/new'
+    else
+      @piece = Piece.create(name: params[:name], size: params[:size], quantity: params[:quantity], pattern: params[:pattern])
+      @piece.user_id = current_user.id
+      @piece.save
+      flash[:message] = "Successfully created piece."
+      erb :"pieces/#{@piece.id}"
+    end
   end
 
   patch '/pieces/:id' do
+    @piece = Piece.find_by_id(params[:id])
+    @piece.update(params[:piece])
+    @piece.save
+
+    redirect :"pieces/#{@piece.id}"
   end
 
   delete '/pieces/:id/delete' do
+    @piece = Piece.find_by_id(params[:id])
   end
 end
