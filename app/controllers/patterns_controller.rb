f62733a0a10bd1230c6136015f583829c222a000
dani8439 <dani8439@aol.com>
2018-11-24 20:57:04 -0500

    Update patterns_controller.rb

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -6,13 +6,6 @@ class PatternsController < ApplicationController
     redirect_if_not_logged_in
     @patterns = current_user.patterns
     erb :'/patterns/index'
-    # @user = User.find_by(params[:id])
-    # @pattern = Pattern.all
-    # if logged_in?
-    #   erb :'/patterns/index'
-    # else
-    #   redirect :'/'
-    # end
   end
 
   get '/patterns/new' do
@@ -49,14 +42,11 @@ class PatternsController < ApplicationController
       redirect :'/patterns/new'
     else
       @pattern = Pattern.create(name: params[:pattern][:name])
-      # @user = User.find_by_id(params[:id]) not necessary
       if current_user.patterns.include?(@pattern)
-      # if @user.patterns.include?(@pattern)
         flash[:message] = "Pattern already exists."
       elsif !params[:piece][:name].empty? && !params[:piece][:size].empty?
         @pattern.piece_ids = params[:pieces]
         @pattern.pieces << Piece.create(name: params[:piece][:name], size: params[:piece][:size], user_id: session[:user_id])
-        # @pattern.save not necessary here as already saved in create..
       else
         @pattern.piece_ids = params[:pieces]
         @pattern.user_id = session[:user_id]
@@ -70,7 +60,6 @@ class PatternsController < ApplicationController
   patch '/patterns/:id' do
     # binding.pry
     redirect_if_not_logged_in
-    # @user = User.find_by(params[:user_id])
     @pattern = Pattern.find_by_id(params[:id])
     if @pattern && @pattern.user_id == current_user.id
       if @pattern.update(name: params[:pattern][:name], user_id: session[:user_id])
